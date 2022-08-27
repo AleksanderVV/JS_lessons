d2a9a2a73d03246e7ce50ed5a95c6513eed8638d
Aleksander Voroniuk <voronyuk.aleksander@gmail.com>
2022-08-27 16:28:58 +0300

    change XMLHttpRequest() на Fetch

--- lesson 64,69,72-73,79,84/js/script.js
+++ lesson 64,69,72-73,79,84/js/script.js
@@ -233,10 +233,12 @@ window.addEventListener('DOMContentLoaded', () => {
                 // form.append(statusMessage);
                 form.insertAdjacentElement('afterend', statusMessage);
 
-                const request = new XMLHttpRequest();
-                request.open('POST', 'server.php');
+                //  ============================== меняю XMLHttpRequest() на Fetch =====================
 
-                request.setRequestHeader('Content-type', 'application/json');
+                // const request = new XMLHttpRequest();
+                // request.open('POST', 'server.php');
+
+                // request.setRequestHeader('Content-type', 'application/json');
                 const formData = new FormData(form);
 
                 const object = {};
@@ -245,20 +247,38 @@ window.addEventListener('DOMContentLoaded', () => {
                 });
                 const json = JSON.stringify(object);
 
-                request.send(json);
-
-                request.addEventListener('load', () => {
-                    if (request.status === 200) {
-                        console.log(request.response);
+                fetch('server.php', {
+                        method: 'POST',
+                        headers: {
+                            'Content-type': 'application/json'
+                        },
+                        body: JSON.stringify(object)
+                    }).then(data => data.text())
+                    .then(data => {
+                        console.log(data);
                         showThanksModal(message.success);
                         form.reset();
-                        setTimeout(() => {
-                            statusMessage.remove();
-                        }, 2000);
-                    } else {
+                        statusMessage.remove();
+                    }).catch(() => {
                         showThanksModal(message.failure);
-                    }
-                });
+                    }).finally(() => {
+                        form.reset();
+                    });
+
+                // request.send(json);
+
+                // request.addEventListener('load', () => {
+                //     if (request.status === 200) {
+                //         console.log(request.response);
+                //         showThanksModal(message.success);
+                //         form.reset();
+                //         setTimeout(() => {
+                //             statusMessage.remove();
+                //         }, 2000);
+                //     } else {
+                //         showThanksModal(message.failure);
+                //     }
+                // });
             });
         }
 
