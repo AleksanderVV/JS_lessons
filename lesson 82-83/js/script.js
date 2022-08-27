8799c99c05cc9840190d95cec703c33335b266aa
Aleksander Voroniuk <voronyuk.aleksander@gmail.com>
2022-08-27 10:28:44 +0300

    added lesson 83

---
+++ lesson 82-83/js/script.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const inputRub = document.querySelector('#rub'),
+    inputUsd = document.querySelector('#usd');
+
+inputRub.addEventListener('input', () => {
+    const request = new XMLHttpRequest();
+
+    request.open('GET', 'js/current.json');
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    request.send();
+
+    // status - статус сервера (200, 404, 500 ....)
+    // statusText - название статуса (ОК, Страница не найдена, Ошибка сервера .....)
+    // response - ответ сервера
+    //
+    //
+
+    // request.addEventListener('readystatechange', () => { // срабатывает при вводе каждой цифры
+    //     if (request.readyState === 4 && request.status === 200) {
+    //         console.log(request.response);
+    //         const data = JSON.parse(request.response);
+    //         inputUsd.value = (+inputRub.value / data.current.usd).toFixed(2);
+    //     } else {
+    //         inputUsd.value = 'Что-то пошло не так';
+    //     }
+    // });
+
+    request.addEventListener('load', () => {
+        if (request.readyState === 4 && request.status === 200) {
+            console.log(request.response);
+            const data = JSON.parse(request.response);
+            inputUsd.value = (+inputRub.value / data.current.usd).toFixed(2);
+        } else {
+            inputUsd.value = 'Что-то пошло не так';
+        }
+    });
+
+});
