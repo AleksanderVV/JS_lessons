95317972a2c47a7be3b29281ee141a009db681e3
Aleksander Voroniuk <voronyuk.aleksander@gmail.com>
2022-09-08 18:14:07 +0300

    modified lesson 114

--- lesson 114 - EventLoop/script.js
+++ lesson 114 - EventLoop/script.js
@@ -1,13 +1,25 @@
 'use strict';
 
-console.log(1);
+// console.log(1);
 
-setTimeout(() => {
-    console.log('timeout-2s');
-}, 2000);
+// setTimeout(() => {
+//     console.log('timeout-2s');
+// }, 2000);
 
-setTimeout(() => {
-    console.log('timeout-4s');
-}, 4000);
+// setTimeout(() => {
+//     console.log('timeout-4s');
+// }, 4000);
 
-console.log(2);
+// console.log(2);
+
+
+
+function foo(a, b) {
+    const [first, second] = a;
+    const { eng, ru } = b;
+    console.log({ eng, ru });
+
+    return `${second}, ${ru}`;
+}
+
+const result = foo(['Hello', 'Привет'], { ru: 'Мир', eng: 'World' })
