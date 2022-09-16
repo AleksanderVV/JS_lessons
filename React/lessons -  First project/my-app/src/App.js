cd6326c35943cfc25c1d90af3a797d2ee536f406
Aleksander Voroniuk <voronyuk.aleksander@gmail.com>
2022-09-16 12:08:36 +0300

    learned setState

--- React/lessons -  First project/my-app/src/App.js
+++ React/lessons -  First project/my-app/src/App.js
@@ -33,23 +33,42 @@ function Btn() {
 
   return <button>{logged ? 'Enter' : text}</button>
 }
-function WhoAmI ({name, surname, link}) {
-  return (
-    <div>
-      <h1>My name is {name()}, surname - {surname}</h1>
-      <a href={link}>Link</a>
-    </div>
-  )
+
+class WhoAmI extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      years: 27,
+      text: '+++'
+    }
+  }
+
+  nextYear = () => {
+    this.setState(state => ({
+        years: state.years + 1
+    }))
+  }
+
+  render() {
+    const {name, surname, link} = this.props;
+    return (
+      <div>
+        <button onClick={this.nextYear}>{this.state.text}</button>
+        <h1>My name is {name}, surname - {surname}, age - {this.state.years}</h1>
+        <a href={link}>Link</a>
+      </div>
+    )
+  }
 }
 
 function App() {
     return ( 
       <div className = "App" >
-        <Header/>
+        {/* <Header/>
         <Field/>
-        <Btn/>
-        <WhoAmI name={() => {return 'John'}} surname="Smith" link="facebook.com"/>
-        <WhoAmI name={() => {return 'John'}} surname="SmithY" link="facebook.com"/>
+        <Btn/> */}
+        <WhoAmI name= 'John' surname="Smith" link="facebook.com"/>
+        <WhoAmI name= 'Alex' surname="SmithY" link="facebook.com"/>
       </div>
     );
 }
